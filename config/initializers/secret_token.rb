e9b994e18532e761ea0136cbd744aa09ede1a2e6
abhishek <abhishek.jain@vinsol.com>
2014-06-17 18:07:32 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpreeBankTransferDemo::Application.config.secret_key_base = '396dabe4993737215df599bb995aa1af2c85cff37b5c6db93988ebed9e539e44efc30210b5d6ed66d1597ddd413718e37357068d97b6922eac09d9231e846d1e'
